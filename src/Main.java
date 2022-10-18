fa4c86eaf11ad172cc0316956f01f0b0b5366c62
Hieu Bui <buiminhhieu301@gmail.com>
2022-10-18 16:28:39 +0700

    HieuBM update code

--- src/Main.java
+++ src/Main.java
@@ -4,25 +4,25 @@ import static org.junit.Assert.*;
 public class Main {
 
     public static String checkStudentGraduated(int credits, double gpa, double ielts) {
-        if(credits < 0 ) {
-            return "Đầu vào không hợp lệ";
+        if(credits < 0 ) {                                                      //1
+            return "Đầu vào không hợp lệ";                                      //2
         }
-        if(gpa < 0.0 || gpa > 4.0) {
-            return "Đầu vào không hợp lệ";
+        if(gpa < 0.0 || gpa > 4.0) {                                            //3
+            return "Đầu vào không hợp lệ";                                      //4
         }
-        if(ielts < 0.0 || ielts > 9.0) {
-            return "Đầu vào không hợp lệ";
+        if(ielts < 0.0 || ielts > 9.0) {                                        //5
+            return "Đầu vào không hợp lệ";                                      //6
         }
-        if(credits < 154) {
-            return "Sinh viên không thể tốt nghiệp";
+        if(credits < 154) {                                                     //7
+            return "Sinh viên không thể tốt nghiệp";                            //8
         }
-        if(gpa < 2.5) {
-            return "Sinh viên không thể tốt nghiệp";
+        if(gpa < 2.5) {                                                         //9
+            return "Sinh viên không thể tốt nghiệp";                            //10
         }
-        if(ielts < 5.5) {
-            return "Sinh viên không thể tốt nghiệp";
+        if(ielts < 5.5) {                                                       //11
+            return "Sinh viên không thể tốt nghiệp";                            //12
         }
-        return "Sinh viên đạt yêu cầu tốt nghiệp";
+        return "Sinh viên đạt yêu cầu tốt nghiệp";                              //13
     }
 
 
---
+++ src/Test3.java
@@ -0,0 +1,54 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class Test3 {
+    @Test
+    public void test1() {
+        int credits = -2;
+        double gpa = 2.0;
+        double ielts = 4.0;
+        assertEquals("Đầu vào không hợp lệ", Main.checkStudentGraduated(credits, gpa, ielts));
+    }
+    @Test
+    public void test2() {
+        int credits = 100;
+        double gpa = 4.1;
+        double ielts = 5.0;
+        assertEquals("Đầu vào không hợp lệ", Main.checkStudentGraduated(credits, gpa, ielts));
+    }@Test
+    public void test3() {
+        int credits = 100;
+        double gpa = 3.0;
+        double ielts = 9.1;
+        assertEquals("Đầu vào không hợp lệ", Main.checkStudentGraduated(credits, gpa, ielts));
+    }
+    @Test
+    public void test4() {
+        int credits = 120;
+        double gpa = 3.0;
+        double ielts = 6.5;
+        assertEquals("Sinh viên không thể tốt nghiệp", Main.checkStudentGraduated(credits, gpa, ielts));
+    }
+    @Test
+    public void test5() {
+        int credits = 155;
+        double gpa = 2.4;
+        double ielts = 6.5;
+        assertEquals("Sinh viên không thể tốt nghiệp", Main.checkStudentGraduated(credits, gpa, ielts));
+    }
+    @Test
+    public void test6() {
+        int credits = 155;
+        double gpa = 2.6;
+        double ielts = 5.0;
+        assertEquals("Sinh viên không thể tốt nghiệp", Main.checkStudentGraduated(credits, gpa, ielts));
+    }
+    @Test
+    public void test7() {
+        int credits = 155;
+        double gpa = 2.6;
+        double ielts = 6.6;
+        assertEquals("Sinh viên đạt yêu cầu tốt nghiệp", Main.checkStudentGraduated(credits, gpa, ielts));
+    }
+}
